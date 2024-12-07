48149cdb0108687cf35007002d6f93872a160862
Navin grg <grgnabin60@gmail.com>
2024-12-07 17:06:44 +0545

    Fix bugs

--- src/components/sections/Topbar.tsx
+++ src/components/sections/Topbar.tsx
@@ -1,41 +1,10 @@
-import React, { useEffect, useState } from 'react';
+"use client";
+import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Topbar = () => {
-    const { data: session, status } = useSession();
-    console.log(session);
-
-    const router = useRouter();
-    const [loading, setLoading] = useState(true); // Track loading state
-
-    useEffect(() => {
-        if (status === "loading") {
-        setLoading(true); // Wait until session is loaded
-        } else {
-        setLoading(false); // Session is loaded, stop loading
-        }
-    }, [status]);
-
-    const user = session?.user;
-
-    const Logout = async () => {
-        await signOut();
-        router.push('/api/auth/signin');
-    };
-
-    const handleAuth = () => {
-        if (user) {
-            Logout();
-        } else {
-            signIn(); // This will use the default provider if you don't pass parameters
-        }
-    };
-
-    // Check if session is still loading before rendering
-    if (loading) return <div>Loading...</div>;
-
+    const route = useRouter();
     return (
         <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         <header className="fixed top-0 left-0 w-full bg-white/10 backdrop-blur-md shadow-md">
@@ -45,7 +14,7 @@ const Topbar = () => {
                 <div className="bg-green-500 h-8 w-8 rounded-full flex items-center justify-center">
                 <span className="text-xl font-bold text-black">BA</span>
                 </div>
-                <span className="text-lg font-semibold">Builders Academy</span>
+                <span onClick={() => route.push('/')} className="text-lg font-semibold">Builders Academy</span>
             </div>
 
             {/* Navigation */}
@@ -60,10 +29,10 @@ const Topbar = () => {
                     Careers
                 </Link>
                 <button
+                onClick={() => route.push('/login')}
                 className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded-full"
-                onClick={handleAuth}
                 >
-                    {user ? "Logout" : "Login"}
+                    Login
                 </button>
             </nav>
             </div>
